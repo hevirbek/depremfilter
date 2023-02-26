eb5278415a876d576d3ee80631a44cf63ffdece6
Hüseyin Averbek <72341654+hevirbek@users.noreply.github.com>
2023-02-26 12:46:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from utils import get_earthquake_data, add_distance_column, filter_data, add_my_location, HiddenPrints
+from utils import get_earthquake_data, add_distance_column, filter_data, add_my_location,get_cities, HiddenPrints
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_ws_localstorage import injectWebsocketCode, getOrCreateUID
 
-# Main call to the api, returns a communication object
+st.set_page_config(page_title="Deprem Filtreleme Uygulaması", page_icon=":earth_americas:", layout="wide", initial_sidebar_state="expanded")
+
 with HiddenPrints():
     conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
 
 database = get_earthquake_data()
+cities = get_cities(database)
+
 
 st.title("Deprem Filtreleme Uygulaması")
 
 min_mag = st.slider(label="Minimum Büyüklük", min_value=0.0, max_value=10.0, value=3.0, step=0.1)
 max_dist = st.slider(label="Maksimum Uzaklık", min_value=0, max_value=1670, value=200, step=10)
+city = st.selectbox(label="Şehir", options=cities)
 
 template = """
 <script type="text/javascript">
@@ -42,12 +46,16 @@ if submit:
             st.session_state['lat'] = float(lat)
             st.session_state['lon'] = float(lon)
 
+    if 'lat' not in st.session_state or 'lon' not in st.session_state:
+        st.error("Konum bilgileri alınamadı. Lütfen konum bilgilerinize izin verin.")
+        st.stop()
+
     st.text(f"Enlem: {st.session_state['lat']}")
     st.text(f"Boylam: {st.session_state['lon']}")
 
     data = database.copy()
     data = add_distance_column(data, st.session_state['lat'], st.session_state['lon'])
-    data = filter_data(data, min_mag, max_dist)
+    data = filter_data(data, min_mag, max_dist, city)
 
     st.table(data)
 
--- utils.py
+++ utils.py
@@ -6,15 +6,12 @@ from datetime import datetime
 import sys
 import os
 
-# Approximate radius of earth in km
+# Dünyanın yaklaşık yarıçapı
 R = 6373.0
 
-# calculate distance between two points
+# iki nokta arasındaki mesafeyi hesaplayan fonksiyon
 def calculate_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
-    lat1 = radians(lat1)
-    lon1 = radians(lon1)
-    lat2 = radians(lat2)
-    lon2 = radians(lon2)
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
     
     dlon = lon2 - lon1
     dlat = lat2 - lat1
@@ -29,13 +26,14 @@ def calculate_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> fl
 
 # Earthquake class
 class Earthquake:
-    def __init__(self, datetime: datetime, latitude: float, longitude: float, depth: float, magnitude: float, region: str, feature: str):
+    def __init__(self, datetime: datetime, latitude: float, longitude: float, depth: float, magnitude: float, region: str, city:str, feature: str):
         self.date = datetime
         self.latitude = latitude
         self.longitude = longitude
         self.depth = depth
         self.magnitude = magnitude
         self.region = region
+        self.city = city
         self.feature = feature
 
 
@@ -45,7 +43,6 @@ def get_earthquake_data() -> pd.DataFrame:
 
     content = requests.get(url).content
     soup = BeautifulSoup(content, "html.parser")
-
     pre = soup.find("pre")
     split_by_newline = pre.text.split("\n")[7:]
 
@@ -62,18 +59,16 @@ def get_earthquake_data() -> pd.DataFrame:
         latitude = float(splitted[2].strip())
         longitude = float(splitted[3].strip())
         depth = float(splitted[4].strip())
-        md = splitted[5].strip()
         ml = float(splitted[6].strip())
-        mw = splitted[7].strip()
         magnitude = ml 
         region = " ".join(splitted[8:-1]).strip()
+        city = region[region.find("(")+1:region.find(")")]
+        region = region[:region.find("(")].strip()
         feature = splitted[-1].strip()
 
-        earthquake = Earthquake(datetime.combine(date, time), latitude, longitude, depth, magnitude, region, feature)
-
+        earthquake = Earthquake(datetime.combine(date, time), latitude, longitude, depth, magnitude, region,city, feature)
         earthquakes.append(earthquake)
 
-    # convert to dataframe
     df = pd.DataFrame([vars(e) for e in earthquakes])
 
     return df
@@ -84,16 +79,28 @@ def add_distance_column(df: pd.DataFrame, lat: float, lon: float) -> pd.DataFram
 
     return df
 
-def filter_data(df: pd.DataFrame, min_magnitude: float, min_distance: float) -> pd.DataFrame:
+def filter_data(df: pd.DataFrame, min_magnitude: float, min_distance: float, city: str) -> pd.DataFrame:
     df = df[df["distance"] < min_distance]
     df = df[df["magnitude"] > min_magnitude]
 
+    if city != "HEPSİ":
+        df = df[df["city"] == city]
+        
     return df
 
 
 def add_my_location(df: pd.DataFrame, lat: float, lon: float) -> pd.DataFrame:
     df = pd.concat([df, pd.DataFrame([[lat, lon, 0.0, 0.0, "My Location", "My Location"]], columns=["latitude", "longitude", "depth", "magnitude", "region", "feature"])], ignore_index=True)
     return df
+
+
+
+def get_cities(df: pd.DataFrame) -> list:
+    cities = df["city"].unique()
+    cities.sort()
+    cities_list = list(cities.copy())
+    cities_list.insert(0, "HEPSİ")
+    return cities_list
 
 
 class HiddenPrints:
@@ -104,3 +111,5 @@ class HiddenPrints:
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
+
+
